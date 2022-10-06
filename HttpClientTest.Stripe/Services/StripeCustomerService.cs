20041a84b9f766b19939e36a759e7b83fdb5c302
Wojciech PIETRZAK <wojciech.pietrzak@swiftcover.com>
2022-10-06 16:02:23 +0200

    fix

--- HttpClientTest.Stripe/Services/StripeCustomerService.cs
+++ HttpClientTest.Stripe/Services/StripeCustomerService.cs
@@ -1,20 +1,22 @@
 ﻿namespace HttpClientTest.Stripe.Services
 {
     using global::Stripe;
+    using System.Net.Http;
 
     internal class StripeCustomerService : ICustomerService
     {
+        const string apiKey = "sk_test_51J6CwAC8DfhOXr8SxF9MqPEOFlmxlDOm3EFQEhv2FG3SMTPmZw1LW3GSsxQqb6dYcxPgrz3QVzqgfPVmQk08cfli00NvfDM3xk";
+
         private readonly IStripeClient stripeClient;
 
-        public StripeCustomerService(IStripeClient stripeClient)
+        public StripeCustomerService(HttpClient httpClient)
         {
-            this.stripeClient = stripeClient;
+            this.stripeClient = new StripeClient(apiKey, httpClient: new SystemNetHttpClient(httpClient));
         }
 
         public string ListCustomers(int limit = 10)
         {
             var service = new CustomerService(stripeClient);
-
             var options = new CustomerListOptions
             {
                 Limit = 2,
--- HttpClientTest.Stripe/Startup.cs
+++ HttpClientTest.Stripe/Startup.cs
@@ -1,6 +1,5 @@
 ﻿namespace HttpClientTest.Stripe
 {
-    using global::Stripe;
     using HttpClientTest.Stripe.Services;
     using Microsoft.Extensions.DependencyInjection;
     using System;
@@ -8,25 +7,11 @@
 
     public sealed class Startup
     {
-        const string apiKey = "sk_test_51J6CwAC8DfhOXr8SxF9MqPEOFlmxlDOm3EFQEhv2FG3SMTPmZw1LW3GSsxQqb6dYcxPgrz3QVzqgfPVmQk08cfli00NvfDM3xk";
-        
         public static IServiceProvider ConfigureServices()
         {
             var services = new ServiceCollection();
 
-            services.AddHttpClient("Stripe");
-            services.AddTransient<IStripeClient, StripeClient>(s =>
-            {
-                var clientFactory = s.GetRequiredService<IHttpClientFactory>();
-                var httpClient = new SystemNetHttpClient(
-                   httpClient: clientFactory.CreateClient("Stripe"),
-                   maxNetworkRetries: StripeConfiguration.MaxNetworkRetries,
-                   enableTelemetry: StripeConfiguration.EnableTelemetry);
-
-                return new StripeClient(apiKey: apiKey, httpClient: httpClient);
-            });
-
-            services.AddSingleton<ICustomerService, StripeCustomerService>();
+            services.AddHttpClient<ICustomerService, StripeCustomerService>(httpClient => new StripeCustomerService(httpClient));
                 
             return services.BuildServiceProvider();
         }
