0866c756dd34e1a83a49bec0a19249c6a5626612
Wojciech PIETRZAK <wojciech.pietrzak@swiftcover.com>
2022-10-06 13:35:48 +0200

    Add solution and console project

---
+++ HttpClientTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HttpClientTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
