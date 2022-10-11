1cdd36a876bc3de6faa87489e853a68f795d7585
SAWICKI Sebastian <sebastian.sawicki@axadirect-solutions.pl>
2022-10-11 15:20:52 +0200

    SingleCall + Benchmark

--- HttpClientTest.Cybersource/Program.cs
+++ HttpClientTest.Cybersource/Program.cs
@@ -1,22 +1,49 @@
 ﻿using HttpClientTest.Cybersource.Model;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace HttpClientTest.Cybersource
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static async Task Main()
+        {
+            //await SingleCallAsync();
+            await BenchmarkAsync();
+            Console.ReadKey();
+        }
+
+        private static async Task SingleCallAsync()
         {
             var serviceProvider = Startup.ConfigureServices();
 
             var service = serviceProvider.GetService<ICybersourceService>();
-
             var request = new GeneratePublicKeyRequest("RsaOaep256", "https://www.test.com");
-
-            var result = service.GeneratePublicKeyAsync("JWT", request).Result;
+            var result = await service.GeneratePublicKeyAsync("JWT", request);
 
             Console.WriteLine(result);
+        }
+
+        private static async Task BenchmarkAsync()
+        {
+            var serviceProvider = Startup.ConfigureServices();
+
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            for (int i = 0; i < 100; i++)
+            {
+                var request = new GeneratePublicKeyRequest("RsaOaep256", "https://www.test.com");
+                var service = serviceProvider.GetService<ICybersourceService>();
+                var result = await service.GeneratePublicKeyAsync("JWT", request);
+
+                Console.WriteLine(i);
+            }
+
+            stopWatch.Stop();
+            Console.WriteLine($"Elapsed = {stopWatch.ElapsedMilliseconds}");
         }
     }
 }
--- HttpClientTest.Stripe/Program.cs
+++ HttpClientTest.Stripe/Program.cs
@@ -3,17 +3,44 @@
     using HttpClientTest.Stripe.Services;
     using Microsoft.Extensions.DependencyInjection;
     using System;
-    
+    using System.Diagnostics;
+    using System.Threading.Tasks;
+
     public class Program
     {
-        public static void Main()
+        public static async Task Main()
+        {
+            //await SingleCallAsync();
+            await BenchmarkAsync();
+            Console.ReadKey();
+        }
+
+        private static async Task SingleCallAsync()
         {
             var serviceProvider = Startup.ConfigureServices();
 
             var customerService = serviceProvider.GetService<ICustomerService>();
-            var result = customerService.ListCustomersAsync().Result;
+            var result = await customerService.ListCustomersAsync();
 
             Console.WriteLine(result);
+        }
+
+        private static async Task BenchmarkAsync()
+        {
+            var serviceProvider = Startup.ConfigureServices();
+
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            for (int i = 0; i < 100; i++)
+            {
+                var customerService = serviceProvider.GetService<ICustomerService>();
+                var result = await customerService.ListCustomersAsync();
+                Console.WriteLine(i);
+            }
+
+            stopWatch.Stop();
+            Console.WriteLine($"Elapsed = {stopWatch.ElapsedMilliseconds}");
         }
     }
 }
